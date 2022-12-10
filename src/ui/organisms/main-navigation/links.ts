210ddb37479400c532ea1f06cb713ab68aff7f53
joshuanwafor <joshuanwafor01@gmail.com>
2022-12-10 15:19:59 +0100

    updated icons

--- src/ui/organisms/main-navigation/links.ts
+++ src/ui/organisms/main-navigation/links.ts
@@ -7,11 +7,12 @@ import {
   Information,
   User,
   ArchiveBook,
+  Activity,
 } from "iconsax-react";
 import { LinksGroupProps } from "./NavigationLinks";
 
 export const navigationLinks: LinksGroupProps[] = [
-  { label: "Overview", icon: User, link: "/account" },
+  { label: "Overview", icon: Activity, link: "/account" },
   { label: "Order History", icon: ArchiveBook, link: "/account/history" },
   { label: "Lockers", icon: LockCircle, link: "/account/lockers" },
   {
