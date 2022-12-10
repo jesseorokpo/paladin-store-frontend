2f4a65645f18e29c94af9e67dc973bbb81a00658
joshuanwafor <joshuanwafor01@gmail.com>
2022-12-10 13:14:50 +0100

    ok

--- src/config/sdk.ts
+++ src/config/sdk.ts
@@ -8,10 +8,11 @@ import {
 } from "../sdk/catalog";
 import { TOKEN } from "./config";
 
-const API_URL = process.env.API_URL;
+const API_URL = "https://0b4a-197-210-53-67.eu.ngrok.io";
 
 let config = {
   accessToken: TOKEN,
+  basePath: API_URL,
   isJsonMime: (mime: any) => {
     return true;
   },
@@ -21,19 +22,32 @@ let axiosConfig = axios.create({
   baseURL: API_URL,
   headers: {
     "Bypass-Tunnel-Reminder": "bypass",
-    "ngrok-skip-browser-warning":"bypass"
+    "ngrok-skip-browser-warning": "bypass",
   },
 });
 
 export let authController = new AuthApi(config, undefined, axiosConfig);
-export let productApiController = new ProductApi(config, undefined, axiosConfig);
-export let taxonomyApiController = new TaxonomyApi(config, undefined, axiosConfig);
+export let productApiController = new ProductApi(
+  config,
+  undefined,
+  axiosConfig
+);
+export let taxonomyApiController = new TaxonomyApi(
+  config,
+  undefined,
+  axiosConfig
+);
 export let lockerApiController = new LockerApi(config, undefined, axiosConfig);
-export let orderControllerApi = new OrderControllerApi(config, undefined, axiosConfig);
+export let orderControllerApi = new OrderControllerApi(
+  config,
+  undefined,
+  axiosConfig
+);
 
 export function configureClientSDK(token: string) {
   let config = {
     accessToken: token,
+    basePath: API_URL,
     isJsonMime: (mime: any) => {
       return true;
     },
